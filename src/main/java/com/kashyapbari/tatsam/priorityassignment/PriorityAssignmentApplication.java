97cd2f1eb002e439553131e152de4fc844c4420c
kashyapbari <bari.r.kash@gmail.com>
2021-01-18 01:19:03 +0530

    initial configuration complete

---
+++ src/main/java/com/kashyapbari/tatsam/priorityassignment/PriorityAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.kashyapbari.tatsam.priorityassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorityAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorityAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kashyapbari/tatsam/priorityassignment/PriorityAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashyapbari.tatsam.priorityassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriorityAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
